ef52e940d86d38aab17fd52146f1bdb9056700d8
Gazua300 <mesmo245@gmail.com>
2023-05-18 01:05:23 -0300

    finalizando página de formulário

---
+++ fundamentar/forms/script.js
@@ -0,0 +1,9 @@
+
+const axe = document.getElementById('axe')
+const blues = document.getElementById('blues')
+const country = document.getElementById('country')
+
+
+document.getElementById('form').addEventListener('submit', (e)=>{
+    e.preventDefault()
+})
